cf0a49724efa9101c4cb399d4e46e13731fc2164
ArkHelvetios <arkhelvetios@gmail.com>
2023-03-17 13:15:23 +0300

    Fix header, remade nav with JS

---
+++ source/js/navigation.js
@@ -0,0 +1,8 @@
+const navigation = document.querySelector('.navigation');
+const navToggle = navigation.querySelector('.navigation__toggle')
+
+navigation.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  navigation.classList.toggle('navigation--active')
+})
