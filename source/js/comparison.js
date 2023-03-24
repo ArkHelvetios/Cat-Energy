5767b8acb26584f9db37a3bf8e193aade65aa6a7
ArkHelvetios <arkhelvetios@gmail.com>
2023-03-24 19:57:50 +0300

    Compariosn block ready
    Made slider on mobile width AND range on table-desktop.Both work responsive - set the onchange listner by matchMedia. Just for fun. Either use tricky ctyling for range input. Actually Index page is done

--- source/js/comparison.js
+++ source/js/comparison.js
@@ -4,6 +4,60 @@ const picItems = comparison.querySelectorAll('.comparison__picture');
 const btnBefore = comparison.querySelector('.comparison__button--before');
 const btnAfter = comparison.querySelector('.comparison__button--after');
 const compToggle = comparison.querySelector('.comparison__toggle');
+const compRange = comparison.querySelector('.comparison__range');
+
+const tabletWidth = 768;
+const tabletMediaQuery = window.matchMedia(`(min-width: ${tabletWidth}px)`);
+
+tabletMediaQuery.addEventListener('change', () => {
+  if (tabletMediaQuery.matches) {
+    picInner.style.transform = `translateX(0%)`;
+    compToggle.style.transform = `translateX(0%)`;
+  }
+})
+
+picItems.forEach((pic, index) => {
+  const picImage = pic.querySelector('.comparison__image');
+  picImage.addEventListener('dragstart', (evt) => evt.preventDefault());
+
+  pic.addEventListener('pointerdown', pointerDown(index));
+  pic.addEventListener('pointermove', pointerMove);
+  pic.addEventListener('pointerup', pointerUp);
+  pic.addEventListener('pointerleave', pointerUp);
+});
+
+btnBefore.addEventListener('click', () => {
+  if (!tabletMediaQuery.matches) {
+    currentIndex = 0;
+    offset = currentIndex * window.innerWidth * -1;
+    initialPosition = offset;
+    picInner.style.transform = `translateX(${currentIndex * 100 * -1}%)`;
+    compToggle.style.transform = `translateX(${currentIndex * 100}%)`;
+  } else {
+    picItems[0].style.width = '100%';
+    picItems[1].style.width = '0%';
+    compRange.value = 0;
+  }
+})
+
+btnAfter.addEventListener('click', () => {
+  if (!tabletMediaQuery.matches) {
+    currentIndex = picItems.length - 1;
+    offset = currentIndex * window.innerWidth * -1;
+    initialPosition = offset;
+    picInner.style.transform = `translateX(${currentIndex * 100 * -1}%)`;
+    compToggle.style.transform = `translateX(${currentIndex * 100}%)`;
+  } else {
+    picItems[0].style.width = '0%';
+    picItems[1].style.width = '100%';
+    compRange.value = 100;
+  }
+})
+
+compRange.addEventListener('input', () => {
+  picItems[0].style.width = `${(100 - compRange.value)}%`;
+  picItems[1].style.width = `${compRange.value}%`;
+})
 
 let isPressed = false;
 let initialTouch = 0;
@@ -11,57 +65,43 @@ let initialPosition = 0;
 let currentIndex = 0;
 let offset = 0;
 
-picItems.forEach((pic, index) => {
-  const picImage = pic.querySelector('.comparison__image');
-  picImage.addEventListener('dragstart', (evt) => evt.preventDefault());
-
-  pic.addEventListener('pointerdown', (evt) => {
+function pointerDown(index) {
+  return (evt) => {
     isPressed = true;
     initialTouch = evt.clientX;
-    currentIndex = index;
-    picInner.classList.add('comparison__picture-inner--touched');
-  });
 
-  pic.addEventListener('pointermove', (evt) => {
-    if (!isPressed) return;
-    const currentTouch = evt.clientX;
+    if (!tabletMediaQuery.matches) {
+      currentIndex = index;
+      picInner.classList.add('comparison__picture-inner--touched');
+    }
+  }
+};
+
+function pointerMove(evt) {
+  if (!isPressed) return
+  const currentTouch = evt.clientX;
+
+  if (!tabletMediaQuery.matches) {
     offset = initialPosition + currentTouch - initialTouch;
     offset = parseInt(offset);
     picInner.style.transform = `translateX(${offset}px)`;
-  });
-
-  pic.addEventListener('pointerup', pointerUp);
-  pic.addEventListener('pointerleave', pointerUp);
-
-});
+  }
+};
 
 function pointerUp() {
   isPressed = false;
 
-  const delta = offset - initialPosition;
-  if (delta < -50 && currentIndex < picItems.length - 1) currentIndex++;
-  if (delta > 50 && currentIndex > 0) currentIndex--;
+  if (!tabletMediaQuery.matches) {
+    const delta = offset - initialPosition;
+    if (delta < -50 && currentIndex < picItems.length - 1) currentIndex++;
+    if (delta > 50 && currentIndex > 0) currentIndex--;
 
-  offset = currentIndex * window.innerWidth * -1;
-  initialPosition = offset;
-  picInner.style.transform = `translateX(${offset}px)`;
-  compToggle.style.transform = `translateX(${currentIndex * 100}%)`;
+    offset = currentIndex * window.innerWidth * -1;
+    initialPosition = offset;
+
+    picInner.style.transform = `translateX(${currentIndex * 100 * -1}%)`;
+    compToggle.style.transform = `translateX(${currentIndex * 100}%)`;
+  }
 
   picInner.classList.remove('comparison__picture-inner--touched');
 }
-
-btnBefore.addEventListener('click', () => {
-  currentIndex = 0;
-  offset = currentIndex * window.innerWidth * -1;
-  initialPosition = offset;
-  picInner.style.transform = `translateX(${offset}px)`;
-  compToggle.style.transform = `translateX(${currentIndex * 100}%)`;
-})
-
-btnAfter.addEventListener('click', () => {
-  currentIndex = picItems.length - 1;
-  offset = currentIndex * window.innerWidth * -1;
-  initialPosition = offset;
-  picInner.style.transform = `translateX(${offset}px)`;
-  compToggle.style.transform = `translateX(${currentIndex * 100}%)`;
-})
