3be236e819cbedecfc26d636c1487b83393f3e05
ArkHelvetios <arkhelvetios@gmail.com>
2023-04-06 23:57:07 +0300

    Remade custom Yandex map
    Made query match for interactive map that matched only on desktop width

---
+++ source/js/map.js
@@ -0,0 +1,27 @@
+const mapWrapper = document.querySelector('.contacts__map')
+
+const desktopWidth = 1280;
+const desktopMediaQuery = window.matchMedia(`(min-width: ${desktopWidth}px)`);
+
+if (desktopMediaQuery.matches) {
+  ymaps3.ready.then(() => {
+    const map = new ymaps3.YMap(mapWrapper, {
+      location: {
+        center: [30.318006, 59.937103],
+        zoom: 16
+      }
+    })
+
+    const mapPin = document.createElement('div');
+    mapPin.classList.add('contacts__map-pin');
+
+    const mapMarker = new ymaps3.YMapMarker({
+      coordinates: [30.323037, 59.938631],
+      draggable: false
+    }, mapPin);
+
+    map.addChild(new ymaps3.YMapDefaultSchemeLayer());
+    map.addChild(new ymaps3.YMapDefaultFeaturesLayer());
+    map.addChild(mapMarker);
+  })
+}
